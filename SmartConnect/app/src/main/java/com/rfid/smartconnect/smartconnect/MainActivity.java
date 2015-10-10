663eade8a153653b0b0d8bec7c5e46d482221e26
Renuka Challa <challarenuka20@gmail.com>
2015-10-10 22:55:29 +0200

    Merged sonali_write branch to Dev_Renuka branch

--- SmartConnect/app/src/main/java/com/rfid/smartconnect/smartconnect/MainActivity.java
+++ SmartConnect/app/src/main/java/com/rfid/smartconnect/smartconnect/MainActivity.java
@@ -1,13 +1,32 @@
 package com.rfid.smartconnect.smartconnect;
 
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+
+import org.ndeftools.Message;
+import org.ndeftools.Record;
+import org.ndeftools.externaltype.AndroidApplicationRecord;
+import android.annotation.SuppressLint;
+
+import android.app.Activity;
 import android.app.PendingIntent;
-import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothServerSocket;
 import android.bluetooth.BluetoothSocket;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.nfc.FormatException;
+import android.nfc.tech.NdefFormatable;
 import android.nfc.NdefMessage;
 import android.nfc.NdefRecord;
 import android.nfc.NfcAdapter;
@@ -18,30 +37,29 @@ import android.os.Bundle;
 import android.os.ParcelUuid;
 import android.os.Parcelable;
 import android.os.Vibrator;
-import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 
-import org.ndeftools.Record;
-import org.ndeftools.externaltype.AndroidApplicationRecord;
-
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.lang.reflect.Method;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Set;
-import java.util.UUID;
-
-
+@SuppressLint({ "ParserError", "ParserError" })
 public class MainActivity extends ActionBarActivity {
 
-    private final static int REQUEST_ENABLE_BT = 1;
     private static String TAG = MainActivity.class.getSimpleName();
+    private final static int REQUEST_ENABLE_BT = 1;
     protected NfcAdapter nfcAdapter;
     protected PendingIntent nfcPendingIntent;
+    protected NfcAdapter adapter;
+    protected PendingIntent pendingIntent;
+    IntentFilter writeTagFilters[];
+    boolean writeMode;
+    Tag mytag;
+    Context ctx;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -50,19 +68,36 @@ public class MainActivity extends ActionBarActivity {
         nfcPendingIntent = PendingIntent.getActivity(this, 0, new Intent(this, this.getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
         Log.d(TAG, "onResume");
         setContentView(R.layout.activity_main);
-        findViewById(R.id.bind_btn).setOnClickListener(new View.OnClickListener() {
+        ctx=this;
+        Button btnWrite = (Button) findViewById(R.id.write_btn);
+        final TextView message = (TextView)findViewById(R.id.edit_message);
+        btnWrite.setOnClickListener(new View.OnClickListener()
+        {
             @Override
             public void onClick(View v) {
+                try {
+                    if(mytag==null){
+                        Toast.makeText(ctx, ctx.getString(R.string.error_detected), Toast.LENGTH_LONG ).show();
+                    }else{
+                        write(message.getText().toString(),mytag);
+                        Toast.makeText(ctx, ctx.getString(R.string.ok_writing), Toast.LENGTH_LONG ).show();
+                    }
+                } catch (IOException e) {
+                    Toast.makeText(ctx, ctx.getString(R.string.error_writing), Toast.LENGTH_LONG ).show();
+                    e.printStackTrace();
+                } catch (FormatException e) {
+                    Toast.makeText(ctx, ctx.getString(R.string.error_writing) , Toast.LENGTH_LONG ).show();
+                    e.printStackTrace();
+                }
+            }
+        });
 
-                Toast.makeText(MainActivity.this, "bind clicked", Toast.LENGTH_SHORT).show();
-            }
-        });
-        findViewById(R.id.pair_btn).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "pair clicked", Toast.LENGTH_SHORT).show();
-            }
-        });
+        adapter = NfcAdapter.getDefaultAdapter(this);
+        pendingIntent = PendingIntent.getActivity(this, 0, new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+        IntentFilter tagDetected = new IntentFilter(NfcAdapter.ACTION_TAG_DISCOVERED);
+        tagDetected.addCategory(Intent.CATEGORY_DEFAULT);
+        writeTagFilters = new IntentFilter[] { tagDetected };
+
     }
 
     @Override
@@ -78,7 +113,19 @@ public class MainActivity extends ActionBarActivity {
         Log.d(TAG, "onPause");
         disableForegroundMode();
     }
+    private void write(String text, Tag tag) throws IOException, FormatException {
 
+        NdefRecord[] records = { createRecord(text) };
+        NdefMessage  message = new NdefMessage(records);
+        // Get an instance of Ndef for the tag.
+        Ndef ndef = Ndef.get(tag);
+        // Enable I/O
+        ndef.connect();
+        // Write the message
+        ndef.writeNdefMessage(message);
+        // Close the connection
+        ndef.close();
+    }
     @Override
     public void onNewIntent(Intent intent) { //
         Log.d(TAG, "onNewIntent");
@@ -90,7 +137,29 @@ public class MainActivity extends ActionBarActivity {
             new NdefReaderTask().execute(tag);
             readMessages(intent);
             handleBluetooth();
+            mytag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+            Toast.makeText(this, this.getString(R.string.ok_detection) + mytag.toString(), Toast.LENGTH_LONG ).show();
         }
+    }
+
+    private NdefRecord createRecord(String text) throws UnsupportedEncodingException {
+        String lang       = "en";
+        byte[] textBytes  = text.getBytes();
+        byte[] langBytes  = lang.getBytes("US-ASCII");
+        int    langLength = langBytes.length;
+        int    textLength = textBytes.length;
+        byte[] payload    = new byte[1 + langLength + textLength];
+
+        // set status byte (see NDEF spec for actual bits)
+        payload[0] = (byte) langLength;
+
+        // copy langbytes and textbytes into payload
+        System.arraycopy(langBytes, 0, payload, 1,              langLength);
+        System.arraycopy(textBytes, 0, payload, 1 + langLength, textLength);
+
+        NdefRecord recordNFC = new NdefRecord(NdefRecord.TNF_WELL_KNOWN,  NdefRecord.RTD_TEXT,  new byte[0], payload);
+
+        return recordNFC;
     }
 
     public void readMessages(Intent intent) {
@@ -106,7 +175,7 @@ public class MainActivity extends ActionBarActivity {
                         Log.d(TAG, " Record #" + k + " is of class " + records.get(k).getClass().getSimpleName());
                         Record record = records.get(k);
                         TextView textView = (TextView) findViewById(R.id.title);
-                        textView.setText(record.getKey());
+                        //textView.setText(record.text.value);
                         if (record instanceof AndroidApplicationRecord) {
                             AndroidApplicationRecord aar = (AndroidApplicationRecord) record;
                             Log.d(TAG, "Package is " + aar.getPackageName());
@@ -155,6 +224,7 @@ public class MainActivity extends ActionBarActivity {
     public void enableForegroundMode() {
         Log.d(TAG, "enableForegroundMode");
         // foreground mode gives the current active application priority for reading scanned tags
+        writeMode = true;
         IntentFilter tagDetected = new IntentFilter(NfcAdapter.ACTION_TAG_DISCOVERED);
         IntentFilter[] writeTagFilters = new IntentFilter[]{tagDetected};
         nfcAdapter.enableForegroundDispatch(this, nfcPendingIntent, writeTagFilters, null);
@@ -162,6 +232,7 @@ public class MainActivity extends ActionBarActivity {
 
     public void disableForegroundMode() {
         Log.d(TAG, "disableForegroundMode");
+        writeMode = false;
         nfcAdapter.disableForegroundDispatch(this);
     }
 
