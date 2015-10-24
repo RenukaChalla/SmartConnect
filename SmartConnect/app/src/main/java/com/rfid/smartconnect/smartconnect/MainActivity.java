d0d885ac75407334ee47337e3620cfb58ae1fb56
sonali.deo <s.deo@campus.tu-berlin.de>
2015-10-24 22:57:02 +0200

    Temp changes for backup

--- SmartConnect/app/src/main/java/com/rfid/smartconnect/smartconnect/MainActivity.java
+++ SmartConnect/app/src/main/java/com/rfid/smartconnect/smartconnect/MainActivity.java
@@ -17,6 +17,7 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.IntentFilter.MalformedMimeTypeException;
 import android.nfc.FormatException;
 import android.nfc.NdefMessage;
 import android.nfc.NdefRecord;
@@ -40,15 +41,16 @@ import android.bluetooth.BluetoothDevice;
 
 @SuppressLint({ "ParserError", "ParserError" })
 public class MainActivity extends Activity {
-
+    public static final String MIME_TEXT_PLAIN = "text/plain";
     private static String TAG = MainActivity.class.getSimpleName();
     protected NfcAdapter adapter;
-    protected PendingIntent pendingIntent;
-    IntentFilter writeTagFilters[];
+  //  protected PendingIntent pendingIntent;
+  //  IntentFilter writeTagFilters[];
     boolean writeMode;
     Tag mytag;
     Context ctx;
     private TextView mTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -57,65 +59,125 @@ public class MainActivity extends Activity {
         mTextView = (TextView) findViewById(R.id.title3);
         // initialize NFC
         adapter = NfcAdapter.getDefaultAdapter(this);
-        pendingIntent = PendingIntent.getActivity(this, 0, new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
-        IntentFilter tagDetected = new IntentFilter(NfcAdapter.ACTION_TAG_DISCOVERED);
-        tagDetected.addCategory(Intent.CATEGORY_DEFAULT);
-        writeTagFilters = new IntentFilter[] { tagDetected };
+        //    pendingIntent = PendingIntent.getActivity(this, 0, new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+        //    IntentFilter tagDetected = new IntentFilter(NfcAdapter.ACTION_TAG_DISCOVERED);
+        //    tagDetected.addCategory(Intent.CATEGORY_DEFAULT);
+        //    writeTagFilters = new IntentFilter[] { tagDetected};
+
+        findViewById(R.id.reset_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    onReset(mytag);
+                } catch (IOException e) {
+                    Log.e(TAG, "Tag not found" + e);
+                } catch (FormatException e) {
+                    Log.e(TAG, "Improper format" + e);
+                }
+            }
+        });
 
     }
 
     @Override
-    public void onPause(){
+    protected void onNewIntent(Intent intent) {
+        Log.d(TAG, "onNewIntent");
+        ctx = this;
+        if (NfcAdapter.ACTION_TAG_DISCOVERED.equals(intent.getAction())) {
+            TextView textView = (TextView) findViewById(R.id.title2);
+            textView.setText("Hello NFC tag!");
+            //    Toast.makeText(ctx, ctx.getString(R.string.hello_tag), Toast.LENGTH_LONG ).show();
+            mytag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+            Parcelable[] messages = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+            if (messages == null) {
+                try {
+                    onReset(mytag);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (FormatException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                // Read tag
+                readTag(mytag);
+            }
+
+            // Notifications for tag write status
+            Toast.makeText(this, this.getString(R.string.ok_detection) + mytag.toString(), Toast.LENGTH_LONG).show();
+            // write to tag
+            try {
+                write(mytag);
+                Toast.makeText(ctx, ctx.getString(R.string.ok_writing), Toast.LENGTH_LONG).show();
+            } catch (IOException e) {
+                Toast.makeText(ctx, ctx.getString(R.string.error_writing), Toast.LENGTH_LONG).show();
+                e.printStackTrace();
+            } catch (FormatException e) {
+                Toast.makeText(ctx, ctx.getString(R.string.error_writing), Toast.LENGTH_LONG).show();
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void onPause() {
         super.onPause();
         Log.d(TAG, "onPause");
         WriteModeOff();
     }
 
     @Override
-    public void onResume(){
+    public void onResume() {
         super.onResume();
         Log.d(TAG, "onResume");
         WriteModeOn();
     }
 
-    private void WriteModeOn(){
+    private void WriteModeOn() {
         writeMode = true;
-        adapter.enableForegroundDispatch(this, pendingIntent, writeTagFilters, null);
+        setupForegroundDispatch(this, adapter);
+        //adapter.enableForegroundDispatch(this, pendingIntent, writeTagFilters, null);
     }
 
-    private void WriteModeOff(){
+    private void WriteModeOff() {
         writeMode = false;
         adapter.disableForegroundDispatch(this);
     }
 
-    // Function to read from the tag
-    public void readMessages(Intent intent) {
-        Parcelable[] messages = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
-        if (messages != null) {
-            Log.d(TAG, "Found " + messages.length + " NDEF messages");
-            vibrate(); // signal found messages :-)
-            for (int i = 0; i < messages.length; i++) {
-                try {
-                    List<Record> records = new org.ndeftools.Message((NdefMessage) messages[i]);
-                    Log.d(TAG, "Found " + records.size() + " records in message " + i);
-                    for (int k = 0; k < records.size(); k++) {
-                        Log.d(TAG, " Record #" + k + " is of class " + records.get(k).getClass().getSimpleName());
-                        Record record = records.get(k);
-                        TextView textView = (TextView) findViewById(R.id.title);
-                        //textView.setText(record.text.value);
-                        if (record instanceof AndroidApplicationRecord) {
-                            AndroidApplicationRecord aar = (AndroidApplicationRecord) record;
-                            Log.d(TAG, "Package is " + aar.getPackageName());
-                        }
-                    }
-                } catch (Exception e) {
-                    Log.e(TAG, "Problem parsing message", e);
-                }
+    public static void setupForegroundDispatch(final Activity activity, NfcAdapter adapter) {
+        final Intent intent = new Intent(activity.getApplicationContext(), activity.getClass());
+        intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
 
+        final PendingIntent pendingIntent = PendingIntent.getActivity(activity.getApplicationContext(), 0, intent, 0);
+
+        IntentFilter[] filters = new IntentFilter[1];
+        String[][] techList = new String[][]{};
+
+        // Notice that this is the same filter as in our manifest.
+        filters[0] = new IntentFilter();
+        filters[0].addAction(NfcAdapter.ACTION_TAG_DISCOVERED);
+        filters[0].addAction(NfcAdapter.ACTION_NDEF_DISCOVERED);
+        filters[0].addCategory(Intent.CATEGORY_DEFAULT);
+        try {
+            filters[0].addDataType(MIME_TEXT_PLAIN);
+        } catch (MalformedMimeTypeException e) {
+            throw new RuntimeException("Check your mime type.");
+        }
+
+        adapter.enableForegroundDispatch(activity, pendingIntent, filters, techList);
+    }
+
+    private void readTag(Tag tag) {
+        NdefRecord[] records = Ndef.get(tag).getCachedNdefMessage().getRecords();
+        for(NdefRecord ndefRecord : records) {
+            if (ndefRecord.getTnf() == NdefRecord.TNF_WELL_KNOWN && Arrays.equals(ndefRecord.getType(), NdefRecord.RTD_TEXT)) {
+                try {
+                    addText(new String(readRecordText(ndefRecord)), mTextView);
+                } catch (UnsupportedEncodingException e) {
+                    Log.e(TAG, "Unsupported Encoding", e);
+                }
             }
         }
     }
-
     private NdefRecord readRecordN(int n) {
 
         Ndef ndef = Ndef.get(mytag);
@@ -125,7 +187,6 @@ public class MainActivity extends Activity {
         }
 
         NdefMessage ndefMessage = ndef.getCachedNdefMessage();
-
         NdefRecord[] records = ndefMessage.getRecords();
         return records[n];
     }
@@ -150,11 +211,23 @@ public class MainActivity extends Activity {
         ndef.close();
     }
 
+    private void onReset(Tag tag) throws IOException, FormatException {
+        String phoneName = getLocalBluetoothName();
+        NdefRecord[] records = { createRecord("blah"),createRecord("0"),createRecord(phoneName) };
+        NdefMessage  message = new NdefMessage(records);
+        // Get an instance of Ndef for the tag.
+        Ndef ndef = Ndef.get(tag);
+        // Enable I/O
+        ndef.connect();
+        // Write the message
+        ndef.writeNdefMessage(message);
+        writeMode = false;
+        // Close the connection
+        ndef.close();
+    }
+
     // Function to create record for tag
     private NdefRecord createRecord(String deviceName) throws UnsupportedEncodingException {
-        // PackageManager pm = getPackageManager();
-        // System.getProperty("os.version");
-
         String lang       = "en";
         String text = deviceName;
         byte[] textBytes  = text.getBytes();
@@ -172,7 +245,6 @@ public class MainActivity extends Activity {
         System.arraycopy(textBytes, 0, payload, 1 + langLength, textLength);
 
         NdefRecord recordNFC = new NdefRecord(NdefRecord.TNF_WELL_KNOWN,  NdefRecord.RTD_TEXT,  new byte[0], payload);
-        //TODO: if device already in paired devices list, add another record to write
 
         return recordNFC;
     }
@@ -181,13 +253,12 @@ public class MainActivity extends Activity {
         String lang = "en";
         String freq = null;
         int frequency = 0;
-        // record[0]=headset name, record[1]=frequency of use, record[2..]=phone's bluetooth name
+        // record[0]=headset name, record[1]=frequency of use, record[2]=phone's bluetooth name
         NdefRecord freq_block = readRecordN(1);
 
         if (freq_block.getTnf() == NdefRecord.TNF_WELL_KNOWN && Arrays.equals(freq_block.getType(), NdefRecord.RTD_TEXT)) {
             try {
-                frequency = tryParse(readRecordText(freq_block));
-              //  frequency = parseInt(readRecordText(freq_block));
+                frequency = toInteger(readRecordText(freq_block));
                 if(frequency == 0) {
                     frequency += 1;
                 }
@@ -199,7 +270,7 @@ public class MainActivity extends Activity {
                 Log.e(TAG, "Unsupported Encoding", e);
             }
         }
-        String text = freq;
+        String text = "used " + freq + " times";
         byte[] textBytes  = text.getBytes();
         byte[] langBytes  = lang.getBytes("US-ASCII");
         int    langLength = langBytes.length;
@@ -219,7 +290,7 @@ public class MainActivity extends Activity {
         return recordNFC;
     }
 
-    public static int tryParse(String text) {
+    public static int toInteger(String text) {
         try {
             return Integer.parseInt(text);
         } catch (NumberFormatException e) {
@@ -235,33 +306,6 @@ public class MainActivity extends Activity {
 
         // Get the Text
         return new String(payload, languageCodeLength + 1, payload.length - languageCodeLength - 1);
-    }
-
-    @Override
-    protected void onNewIntent(Intent intent){
-        Log.d(TAG, "onNewIntent");
-        ctx=this;
-        if(NfcAdapter.ACTION_TAG_DISCOVERED.equals(intent.getAction())){
-            TextView textView = (TextView) findViewById(R.id.title2);
-            textView.setText("Hello NFC tag!");
-         //   Toast.makeText(ctx, ctx.getString(R.string.hello_tag), Toast.LENGTH_LONG ).show();
-            mytag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
-            // Read tag
-            new NdefReaderTask().execute(mytag);
-         //   readMessages(intent);
-            Toast.makeText(this, this.getString(R.string.ok_detection) + mytag.toString(), Toast.LENGTH_LONG ).show();
-            // write to tag
-            try {
-                write(mytag);
-                Toast.makeText(ctx, ctx.getString(R.string.ok_writing), Toast.LENGTH_LONG ).show();
-            } catch (IOException e) {
-                Toast.makeText(ctx, ctx.getString(R.string.error_writing), Toast.LENGTH_LONG ).show();
-                e.printStackTrace();
-            } catch (FormatException e) {
-                Toast.makeText(ctx, ctx.getString(R.string.error_writing) , Toast.LENGTH_LONG ).show();
-                e.printStackTrace();
-            }
-        }
     }
 
     // Phone's Bluetooth name
@@ -285,71 +329,20 @@ public class MainActivity extends Activity {
         vibe.vibrate(500);
     }
 
+    private void addText(final String theText, final TextView view) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                int length = 20;
+                String newString = view.getText().toString();
+                if (newString.length() > length)
+                    newString = newString.substring(newString.length() - length);
 
-    // Background task for reading the data. Do not block the UI thread while reading.
+                newString += "\n" + theText;
 
-    private class NdefReaderTask extends AsyncTask<Tag, Void, String> {
-        @Override
-        protected String doInBackground(Tag... params) {
-            Tag tag = params[0];
-
-            Ndef ndef = Ndef.get(tag);
-            if (ndef == null) {
-                // NDEF is not supported by this Tag.
-                return null;
+                view.setText(newString);
+                view.invalidate();
             }
-
-            NdefMessage ndefMessage = ndef.getCachedNdefMessage();
-
-            NdefRecord[] records = ndefMessage.getRecords();
-            for (NdefRecord ndefRecord : records) {
-                if (ndefRecord.getTnf() == NdefRecord.TNF_WELL_KNOWN && Arrays.equals(ndefRecord.getType(), NdefRecord.RTD_TEXT)) {
-                    try {
-                        return readText(ndefRecord);
-                    } catch (UnsupportedEncodingException e) {
-                        Log.e(TAG, "Unsupported Encoding", e);
-                    }
-                }
-            }
-
-            return null;
-        }
-
-        private String readText(NdefRecord record) throws UnsupportedEncodingException {
-        /*
-         * See NFC forum specification for "Text Record Type Definition" at 3.2.1
-         *
-         * http://www.nfc-forum.org/specs/
-         *
-         * bit_7 defines encoding
-         * bit_6 reserved for future use, must be 0
-         * bit_5..0 length of IANA language code
-         */
-
-            byte[] payload = record.getPayload();
-
-            // Get the Text Encoding
-            String textEncoding = ((payload[0] & 128) == 0) ? "UTF-8" : "UTF-16";
-
-            // Get the Language Code
-            int languageCodeLength = payload[0] & 0063;
-
-            // String languageCode = new String(payload, 1, languageCodeLength, "US-ASCII");
-            // e.g. "en"
-
-            // Get the Text
-            String stuff = new String(payload, languageCodeLength + 1, payload.length - languageCodeLength - 1);
-         //   mTextView.setText("Tag says: %s" + stuff);
-            return new String(payload, languageCodeLength + 1, payload.length - languageCodeLength - 1);
-        }
-
-        //@Override
-        protected void onPostExecute(String result) {
-            if (result != null) {
-                mTextView.setText("Tag says: " + result);
-            }
-        }
+        });
     }
-
-
 }
